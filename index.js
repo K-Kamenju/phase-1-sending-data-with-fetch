2549e92ef8e0951865888a98d8970b6879ccdaeb
Caleb <kamenjuhkiiru@gmail.com>
2023-10-04 11:01:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+  
+    return fetch("http://localhost:3000/users", config)
+      .then(res => res.json())
+      .then(data => {
+        const idElement = document.createElement("p");
+        idElement.textContent = data.id;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorElement = document.createElement("p");
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
